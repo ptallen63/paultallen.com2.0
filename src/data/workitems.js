e7f83b364e65166212e71c2d3accf9275d9e32cf
Paul Allen <ptallen63@gmail.com>
2017-12-07 21:42:44 -0500

    updating resume

--- src/data/workitems.js
+++ src/data/workitems.js
@@ -1,102 +1,97 @@
 const workitems = [
   {
-    company: 'University of South Carolina',
-    location: 'Columbia, SC',
-    position: 'Marketing Technologist',
-    timeSpan: 'August 2016 - Present',
+    company: "University of South Carolina",
+    location: "Columbia, SC",
+    position: "Marketing Technologist",
+    timeSpan: "August 2016 - Present",
     bullets: [
-      'Find technology solutions to challenges facing the marketing and communication group',
-      'Define, manage, and optimize the marketing technology stack',
-      'Provide guidance on technology systems and solutions to units inside and outside of the university',
+      "Overhauled the analytics infrastructure on all web properties using Google Tag Manager which led to increased flexibility in measurement and testing",
+      "Launched the College Newsletter Program that provided strategies to colleges within the university to successfully launch their newsletters",
+      "Rebuilt email templates that are used at the highest level",
+      "Authored several open source programs related to email reporting and management ",
+      "Found technology solutions to challenges facing the marketing and communications group",
+      "Defined, managed, and optimized the marketing technology stack",
+      "Provided guidance on technology systems and solutions to units inside and outside of the university"
     ]
   },
   {
-    company: 'University of South Carolina, Darla Moore School of Business',
-    position: 'Assitant Director of Recruiting',
-    location: 'Columbia, SC',
-    timeSpan: 'April 2013 - June 2016',
+    company: "University of South Carolina, Darla Moore School of Business",
+    position: "Assitant Director of Recruiting",
+    location: "Columbia, SC",
+    timeSpan: "April 2013 - June 2016",
     bullets: [
-      'Engineered and implemented the communication model for all prospective PMBA students',
-      'Designed and programmed the HTML email templates used throughout the communication model',
-      'Constructed a system to track current budget usage and processing',
-      'Designed and programed all mass email communications sent to current and prospective PMBA  students',
-      'Maintained and utilize the CRM system to enhance current recruiting and reporting efforts',
-      'Managed the information content and user experience of the PMBA website',
-      'Designed and developed systems to market and provide metrics on recruiting events and strategies',
-      'Provided input on broad level marketing decisions for the PMBA program',
-      'Developed and  manage social media strategies  for the PMBA program',
-
+      "Engineered and implemented the communication model for all prospective PMBA students",
+      "Designed and programmed the HTML email templates used throughout the communication model",
+      "Constructed a system to track current budget usage and processing",
+      "Designed and programed all mass email communications sent to current and prospective PMBA  students",
+      "Maintained and utilize the CRM system to enhance current recruiting and reporting efforts",
+      "Managed the information content and user experience of the PMBA website",
+      "Designed and developed systems to market and provide metrics on recruiting events and strategies",
+      "Provided input on broad level marketing decisions for the PMBA program",
+      "Developed and  manage social media strategies  for the PMBA program"
     ]
   },
   {
-    company: 'University of South Carolina, Darla Moore School of Business',
-    position: 'Consultant',
-    location: 'Columbia, SC',
-    timeSpan: 'November 2014 - June 2016',
+    company: "University of South Carolina, Darla Moore School of Business",
+    position: "Consultant",
+    location: "Columbia, SC",
+    timeSpan: "November 2014 - June 2016",
     bullets: [
-      'Led project to investigate potentially merging data systems across the college to leverage relational data',
-      'Provided input into communication strategy for open & custom executive programs',
-      'Advised on  web and communication strategies across the college',
-
-
+      "Led project to investigate potentially merging data systems across the college to leverage relational data",
+      "Provided input into communication strategy for open & custom executive programs",
+      "Advised on  web and communication strategies across the college"
     ]
   },
   {
-    company: 'Southern Wesleyan University',
-    position: 'Admissions Coordinator',
-    location: 'Columbia, SC',
-    timeSpan: 'June 2011 - March 2013',
+    company: "Southern Wesleyan University",
+    position: "Admissions Coordinator",
+    location: "Columbia, SC",
+    timeSpan: "June 2011 - March 2013",
     bullets: [
-      'Developed relationships with businesses to promote the programs for adult and graduate studies and advance the university’s interests',
-      'Started the SWU Ambassador program for adult students to enhance recruiting efforts',
-      'Promoted the university and the AGS program to students through various avenues',
-      'Advised students on programs that match  their educational needs',
-      'Worked with students throughout the enrollment process to obtain all materials and fees required for admission and enrollment',
-
+      "Developed relationships with businesses to promote the programs for adult and graduate studies and advance the university’s interests",
+      "Started the SWU Ambassador program for adult students to enhance recruiting efforts",
+      "Promoted the university and the AGS program to students through various avenues",
+      "Advised students on programs that match  their educational needs",
+      "Worked with students throughout the enrollment process to obtain all materials and fees required for admission and enrollment"
     ]
   },
   {
-
-    position: 'Consultant',
-    location: 'Cairo, Egypt',
-    timeSpan: '2009 - 2011',
+    position: "Consultant",
+    location: "Cairo, Egypt",
+    timeSpan: "2009 - 2011",
     bullets: [
-      'Built and developed relationships with members of the business community',
-      'Researched and implemented methods of conducting business in the Arab world',
-      'Consulted with small, local companies wanting to build Western clientele',
-      'Navigated living and working during the Arab Spring',
-
+      "Built and developed relationships with members of the business community",
+      "Researched and implemented methods of conducting business in the Arab world",
+      "Consulted with small, local companies wanting to build Western clientele",
+      "Navigated living and working during the Arab Spring"
     ]
   },
   {
-    position: 'Assistant Branch Manager',
-    company: 'Regions Bank',
-    location: 'Birmingham, AL',
-    timeSpan: '2007 - 2009',
+    position: "Assistant Branch Manager",
+    company: "Regions Bank",
+    location: "Birmingham, AL",
+    timeSpan: "2007 - 2009",
     bullets: [
-      'Directly managed the sale of all products inside the branch',
-      'Managed and developed  sales staff employees',
-      'Achieved ambitious organizational goals  in the areas of operations, sales, and customer service',
-
+      "Directly managed the sale of all products inside the branch",
+      "Managed and developed  sales staff employees",
+      "Achieved ambitious organizational goals  in the areas of operations, sales, and customer service"
     ]
   },
   {
-    position: 'Financial Services Representative',
-    company: 'Regions Bank',
-    location: 'Birmingham, AL',
-    timeSpan: '2006 - 2007',
-    bullets:[],
-
+    position: "Financial Services Representative",
+    company: "Regions Bank",
+    location: "Birmingham, AL",
+    timeSpan: "2006 - 2007",
+    bullets: []
   },
   {
-    position: 'Internship',
-    company: 'Merrill Lynch',
-    location: 'Birmingham, AL',
-    timeSpan: 'Summer 2005',
-    bullets:[],
-
-  },
-]
+    position: "Internship",
+    company: "Merrill Lynch",
+    location: "Birmingham, AL",
+    timeSpan: "Summer 2005",
+    bullets: []
+  }
+];
 
 
 export default workitems;
